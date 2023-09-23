b3bae0e4edf8b9d5d09dce34d030043e3f9b8e7f
Miguel Angelus <miguelangelus60@gmail.com>
2023-09-23 20:50:09 -0300

    solving a compilation mistake at makefile

--- headerFiles/matrix_lib.h
+++ headerFiles/matrix_lib.h
@@ -11,6 +11,7 @@ int scalar_matrix_mult(float scalar_value, Matrix *matrix);
 int matrix_matrix_mult(Matrix *matrixA, Matrix *matrixB, Matrix *matrixC);
 int matrix_matrix_mult_optimized(Matrix *matrixA, Matrix *matrixB, Matrix *matrixC);
 int matrix_matrix_mult_optimized_vetorial(Matrix *matrixA, Matrix *matrixB, Matrix *matrixC);
+int scalar_matrix_mult_optimized_vetorial(float scalar_value, Matrix *matrix);
 void print_matrix(Matrix *matrix);
 Matrix* matrix_init(int height, int width);
 
